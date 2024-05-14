3d9fb9f83c0cbbc1cb04b7fa91d138ff5a19b399
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-05-14 18:45:40 +0700

    test lgi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
 import HeroSection from '../components/Home/HeroSection'
+import Testing from '../components/Home/Testing'
 
 function Home() {
   return (
     <div>
         <div className='overflow-hidden'>
             <Navbar></Navbar>
-            <HeroSection />
+            {/* <HeroSection /> */}
+            <Testing />
         </div>
     </div>
   )
