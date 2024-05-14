ab6f3e662f4e75c4ae4f5707a5071fdd537bace6
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-05-14 17:08:14 +0700

    fixing semua

---
+++ src/pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+        <div className='text-violet-600'>
+            <h1>HELOO</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Home
