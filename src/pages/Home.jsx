3c5a9bb64805753db64085cfcde621110c28bb30
arrayyan aprilyanto <146199861+arrayyan123@users.noreply.github.com>
2024-05-14 17:37:44 +0700

    lagi

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Navbar from '../components/Navbar'
+import HeroSection from '../components/Home/HeroSection'
 
 function Home() {
   return (
     <div>
         <div className='overflow-hidden'>
             <Navbar></Navbar>
+            <HeroSection />
         </div>
     </div>
   )
