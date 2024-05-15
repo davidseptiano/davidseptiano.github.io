3cfa2b91e0cf0b487f4566e4d025faf6f6bd1aff
NotNBL <nabilrabbani2000@gmail.com>
2024-05-15 20:36:45 +0700

    masukin css login
    navbar nya gajadi

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,25 +1,26 @@
 // src/components/Login/Login.js
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Login.css";
 
 const Login = ({ setUsername }) => {
-  const [name, setName] = useState('');
+  const [name, setName] = useState("");
   const navigate = useNavigate();
 
   const handleLogin = () => {
-    localStorage.setItem('username', name);
+    localStorage.setItem("username", name);
     setUsername(name);
-    navigate('/');
+    navigate("/");
   };
 
   return (
-    <div class='login-container'>
+    <div class="login-container">
       <h2>Login</h2>
-      <input 
-        type="text" 
-        value={name} 
-        onChange={(e) => setName(e.target.value)} 
-        placeholder="Enter your name" 
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Enter your name"
       />
       <button onClick={handleLogin}>Login</button>
     </div>
