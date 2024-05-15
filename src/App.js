ace1579a4eae119928eb182a5b21d827f9bf0276
NotNBL <nabilrabbani2000@gmail.com>
2024-05-15 20:23:56 +0700

    products

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
 // src/App.js
-import React, { useState, useEffect } from 'react';
-import Navbar from './components/Navbar';
-import './App.css';
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from './pages/Home';
-import Services from './pages/Services';
-import Login from './components/Login/Login';
+import React, { useState, useEffect } from "react";
+import Navbar from "./components/Navbar";
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Navigate,
+} from "react-router-dom";
+import Home from "./pages/Home";
+import Services from "./pages/Services";
+import Login from "./components/Login/Login";
+import Products from "./pages/Products";
 
 function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState("");
 
   useEffect(() => {
-    const storedUsername = localStorage.getItem('username');
+    const storedUsername = localStorage.getItem("username");
     if (storedUsername) {
       setUsername(storedUsername);
     }
@@ -22,8 +28,18 @@ function App() {
       <Router>
         <Routes>
           <Route path="/login" element={<Login setUsername={setUsername} />} />
-          <Route path="/" element={username ? <Home /> : <Navigate to="/login" />} />
-          <Route path="/services" element={username ? <Services /> : <Navigate to="/login" />} />
+          <Route
+            path="/"
+            element={username ? <Home /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/services"
+            element={username ? <Services /> : <Navigate to="/login" />}
+          />
+          <Route
+            path="/products"
+            element={username ? <Products /> : <Navigate to="/login" />}
+          />
         </Routes>
       </Router>
     </>
